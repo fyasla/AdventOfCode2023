a6de266b6d180e24b2a8e9103cd311b5d64d984d
fyasla <f.bougamale@gmail.com>
2023-12-16 16:09:19 +0100

    day 04 star 2

--- day04/day04.hs
+++ day04/day04.hs
@@ -1,5 +1,6 @@
-import Data.List.Split
 import Data.List
+import Data.List.Split
+import System.Posix (BaudRate (B0))
 
 -- getScore :: [String] -> [String] -> Int
 -- getScore winning nums = let matches = length $ intersect winning nums
@@ -11,13 +12,20 @@ countMatchingNums winning nums = length $ intersect winning nums
 getCardNumbers :: String -> [[String]]
 getCardNumbers l = map words $ drop 1 $ splitWhen (`elem` ":|") $ head $ lines l
 
-processLine :: String -> Int 
+processLine :: String -> Int
 processLine l = countMatchingNums (head a) (last a) where a = getCardNumbers l
 
-solution :: String -> String
-solution input = (show $ map processLine (lines input)) 
+-- solution :: String -> String
+-- solution input = (show $ map processLine (lines input))
 
-countCards :: [Int] -> Int
-countCards matches = replicate (length matches) 1
+solution' :: String -> String
+solution' input =
+  let ls = map processLine (lines input)
+   in show $ countCards ls (replicate (length ls) 1)
 
-main = interact solution
+-- takes list of mathces and list of cards (initialized to : replicate (length matches) 1)
+countCards :: [Int] -> [Int] -> Int
+countCards matches [] = 0
+countCards (m : ms) (c : cs) = c + countCards ms (zipWith (+) cs (replicate m c ++ [0, 0 ..]))
+
+main = interact solution'
