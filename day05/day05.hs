51e86cd1cacb1e3e0310f2bf64886dd0de57c1a8
fyasla <f.bougamale@gmail.com>
2023-12-17 14:26:57 +0100

    took a good minute to run but at least it ends (compared to using  on [a..b] instead of comparing directly with boundary values

--- day05/day05.hs
+++ day05/day05.hs
@@ -1,9 +1,17 @@
 import Data.List.Split
 
-main = interact solution
+main = interact solution'
 
 getSeeds :: String -> [Int]
 getSeeds = map (read :: String -> Int) . words . last . (splitOn ":") . head . lines
+
+getSeeds' :: [Int] -> [(Int, Int)]
+getSeeds' [] = []
+getSeeds' (s : r : xs) = (s, s + r) : getSeeds' xs
+
+isInSeedsRange :: [(Int, Int)] -> Int -> Bool
+isInSeedsRange [] _ = False
+isInSeedsRange (r : rs) x = (x >= fst r && x < snd r) || isInSeedsRange rs x
 
 parseInput :: String -> [[(Int, Int, Int -> Int)]]
 parseInput = map (map parseLine) . map (map (map (read :: String -> Int))) . map (map words) . map tail . tail . splitOn [""] . lines
@@ -17,8 +25,20 @@ sourceToDest ((low, high, f) : ls) x
 parseLine :: [Int] -> (Int, Int, Int -> Int)
 parseLine [dest, source, n] = (source, source + n, ((dest - source) +))
 
+revParseLine :: [Int] -> (Int, Int, Int -> Int)
+revParseLine [dest, source, n] = (dest, dest + n, ((source - dest) +))
+
+revParseInput :: String -> [[(Int, Int, Int -> Int)]]
+revParseInput = map (map revParseLine) . map (map (map (read :: String -> Int))) . map (map words) . map tail . tail . splitOn [""] . lines
+
 getLocation :: String -> Int -> Int
 getLocation input = foldl (flip (.)) id $ map sourceToDest (parseInput input)
 
+getSeed :: String -> Int -> Int
+getSeed input = foldl (.) id $ map sourceToDest (revParseInput input)
+
 solution :: String -> String
 solution input = show $ minimum $ map (getLocation input) (getSeeds input)
+
+solution' :: String -> String
+solution' input = show $ length $ takeWhile (not . (isInSeedsRange (getSeeds' $ getSeeds input))) $ map (getSeed input) [0 ..]
